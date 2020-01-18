95d748f08762b4ffddd59a3d0e5adb49e900d67d
Anson Foong <ansfoong@gmail.com>
2020-01-18 08:46:34 -0500

    Created model driven form. Submit post request to create forum category.

--- src/app/components/ForumAdminTools/category-create-form/category-create-form.component.ts
+++ src/app/components/ForumAdminTools/category-create-form/category-create-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ForumAdminService } from 'src/app/services/ForumAdminPanel/forum-admin.service';
+import ForumCategory from 'src/app/models/ForumCategory';
 
 @Component({
   selector: 'app-category-create-form',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryCreateFormComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public categoryForm: FormGroup;
+  constructor(private fb: FormBuilder, private forumService: ForumAdminService) { 
+    this.categoryForm = fb.group({
+      title: new FormControl('', Validators.required),
+      moderators: new FormControl('')
+    });
   }
 
+  ngOnInit() {
+
+  }
+  submit() : void {
+    if(this.categoryForm.valid) {
+      // Make HTTP POST
+      let cat: ForumCategory = {
+        name: this.categoryForm.value.title
+      }
+      this.forumService.createForumCategory(cat)
+        .subscribe((res : any) => {
+          console.log(res);
+        }, err => { console.log(err); })
+
+    }
+    else {
+      // Do nothing.
+    }
+  }
 }
