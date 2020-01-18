1ee629925c7a2fdeb2d9a2932ec8fc0e1373a45f
Anson Foong <ansfoong@gmail.com>
2020-01-18 07:16:55 -0500

    Added ForumPage component

---
+++ src/app/components/forum-page/forum-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-page',
+  templateUrl: './forum-page.component.html',
+  styleUrls: ['./forum-page.component.css']
+})
+export class ForumPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
