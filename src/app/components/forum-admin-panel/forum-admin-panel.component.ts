17077e06df67198ff485f32e81e496261296027c
Anson Foong <ansfoong@gmail.com>
2020-01-18 08:26:01 -0500

    display diaog with component on click of button

--- src/app/components/forum-admin-panel/forum-admin-panel.component.ts
+++ src/app/components/forum-admin-panel/forum-admin-panel.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { ForumAdminService } from 'src/app/services/ForumAdminPanel/forum-admin.service';
+import ForumCategory from 'src/app/models/ForumCategory';
+import { MatDialog } from '@angular/material/dialog';
+import { ForumDialogComponent } from '../ReuseableDialogs/forum-dialog/forum-dialog.component';
+import { CategoryCreateFormComponent } from '../ForumAdminTools/category-create-form/category-create-form.component';
 
 @Component({
   selector: 'app-forum-admin-panel',
@@ -8,10 +13,23 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ForumAdminPanelComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(private forumAdminService: ForumAdminService,
+    private dialog: MatDialog) { }
 
   ngOnInit() {
     
   }
 
+  createCategory() : void {
+    // Display Modal with Create Category Form.
+    this.dialog.open(ForumDialogComponent, {
+      data: {
+        component: CategoryCreateFormComponent
+      }
+    })
+  } 
+
+  createTopic() : void {
+
+  }
 }
