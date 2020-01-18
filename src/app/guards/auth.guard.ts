a0be2dac10c247f58e8eb6eb741254152ebb7e9f
Anson Foong <ansfoong@gmail.com>
2020-01-18 07:38:05 -0500

    Return true if authed, false otherwise for auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
+
+  constructor(private auth: AuthService) {}
+  async canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): Promise<boolean> {
+      console.log("Hi")
+      try {
+        await this.auth.isAuthorized().toPromise();
+        console.log("Good");
+        return true;
+      }
+      catch(err) {
+        console.log("Bad");
+        return false;
+      }
   }
   
 }
