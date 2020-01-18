a0b6b7e5359cdce7fb14ec92581f643754d6b5a8
Anson Foong <ansfoong@gmail.com>
2020-01-18 08:42:00 -0500

    Create category in controller for forum category route

--- server/controllers/forum.controller.js
+++ server/controllers/forum.controller.js
@@ -1,8 +1,24 @@
 const ForumCategory = require('../models/forum/ForumCategory');
 const ForumTopic = require('../models/forum/ForumTopic');
 
-const createCategory = (req, res) => {
-
+const createCategory = async (req, res) => {
+    let { name } = req.body;
+    try {
+        let cat = await ForumCategory.findOne({ name });
+        console.log(cat);
+        if(cat) {
+            console.log("Category already exists!");
+            res.status(200).json({ msg: 'Already exists' });
+        }
+        else {
+            let saveCategory = await ForumCategory.create({ name });
+            await saveCategory.save();
+            res.status(201).json({ msg: 'Created!' });
+        }
+    }
+    catch(err) {
+        res.status(500).json({ msg: 'error' });
+    }
 }
 const createTopic = (req, res) => {
 
--- server/routes/forum.js
+++ server/routes/forum.js
@@ -2,5 +2,5 @@ const router = module.exports = require('express').Router();
 const controller = require('../controllers/forum.controller');
 const User = require('../models/User');
 
-router.post('/forum/create/category', controller.createCategory);
-router.post('/forum/create/topic', controller.createTopic);
+router.post('/create/category', controller.createCategory);
+router.post('/create/topic', controller.createTopic);
