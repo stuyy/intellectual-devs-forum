1eb03da1d0e8df996d8fa8233e82d32fc4acbbca
Anson Foong <ansfoong@gmail.com>
2020-01-18 07:16:48 -0500

    Minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForumPageComponent } from './components/forum-page/forum-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'forum', pathMatch: 'full', component: ForumPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ForumPageComponent } from './components/forum-page/forum-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForumPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
