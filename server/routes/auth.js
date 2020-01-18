7be6d13a962ce90b038db7b6f7e099744727efe3
Anson Foong <ansfoong@gmail.com>
2020-01-18 07:00:23 -0500

    added auth route and auth controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,1 +1,10 @@
 const router = module.exports = require('express').Router();
+const passport = require('passport');
+const User = require('../models/User');
+
+router.get('/discord', passport.authenticate('discord'));
+router.get('/discord/redirect', passport.authenticate('discord', {
+    failureRedirect: '/'
+}), function(req, res) {
+    console.log("Good")
+});
