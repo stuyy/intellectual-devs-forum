17229fbbbfa0e1ae6c00eed86105ad3ec60b9cc1
Anson Foong <ansfoong@gmail.com>
2020-01-25 13:55:03 -0500

    render forum topics under categories

--- src/app/components/Forum/forum-topic/forum-topic.component.ts
+++ src/app/components/Forum/forum-topic/forum-topic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import ForumTopic from 'src/app/models/ForumTopic';
 
 @Component({
   selector: 'app-forum-topic',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForumTopicComponent implements OnInit {
 
+  @Input() forumTopic: ForumTopic;
+  public url: string;
+  public category: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.url = this.forumTopic.name.toLowerCase().replace(new RegExp(/\s/g), '-');
+    this.category = this.forumTopic.parentCategory.toLowerCase().replace(new RegExp(/\s/g), '-');
   }
 
 }
