675f06f40ca7f3c8d24f10fa3c712292fbb17a49
Anson Foong <ansfoong@gmail.com>
2020-01-25 18:09:24 -0500

    Added function createPost to make HTTP post call. Added postDialogEvents for EventEMitter

--- src/app/services/ForumService/forum.service.ts
+++ src/app/services/ForumService/forum.service.ts
@@ -5,11 +5,14 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import ForumPost from 'src/app/models/ForumPost';
 import ForumTopic from 'src/app/models/ForumTopic';
+import { PostEvent } from 'src/app/models/Events/PostEvent';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForumService {
+
+  public postDialogEvents: EventEmitter<PostEvent> = new EventEmitter<PostEvent>();
 
   constructor(private http: HttpClient) { }
 
@@ -21,5 +24,8 @@ export class ForumService {
   }
   getForumPosts(category, topic) : Observable<Array<ForumPost>> {
     return this.http.get<Array<ForumPost>>(`${environment.host}/forum/posts/${category}/${topic}`);
+  }
+  createPost(post: ForumPost) : Observable<ForumPost> {
+    return this.http.post<ForumPost>(`${environment.host}/forum/posts/create/`, post, { withCredentials: true });
   }
 }
