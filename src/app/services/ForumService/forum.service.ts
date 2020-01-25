adf1d04400fa1120cb55b7a6cf2fec15ca347982
Anson Foong <ansfoong@gmail.com>
2020-01-25 13:54:29 -0500

    Added ForumTopic and ForumPosts functions to retrieve from server

--- src/app/services/ForumService/forum.service.ts
+++ src/app/services/ForumService/forum.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import ForumCategory from 'src/app/models/ForumCategory';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import ForumPost from 'src/app/models/ForumPost';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,11 @@ export class ForumService {
 
   getForumCategories() : Observable<Array<ForumCategory>> {
     return this.http.get<Array<ForumCategory>>(`${environment.host}/forum/categories`);
+  }
+  getForumTopics(name) : Observable<Array<ForumCategory>> {
+    return this.http.get<Array<ForumCategory>>(`${environment.host}/forum/topics/${name}`);
+  }
+  getForumPosts(category, topic) : Observable<Array<ForumPost>> {
+    return this.http.get<Array<ForumPost>>(`${environment.host}/forum/posts/${category}/${topic}`);
   }
 }
