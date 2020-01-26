9c5fdf0860053d14bfab50b4eb0eab410556df40
Anson Foong <ansfoong@gmail.com>
2020-01-25 21:13:28 -0500

    Fetch posts, render dynamic data using AsyncPipe

--- src/app/components/Forum/forum-topic-page/forum-topic-page.component.ts
+++ src/app/components/Forum/forum-topic-page/forum-topic-page.component.ts
@@ -6,6 +6,10 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CreatePostFormComponent } from '../../create-post-form/create-post-form.component';
 import { PostEvent } from 'src/app/models/Events/PostEvent';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { share } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-forum-topic-page',
@@ -15,13 +19,18 @@ import { PostEvent } from 'src/app/models/Events/PostEvent';
 export class ForumTopicPageComponent implements OnInit {
 
   public currentPage: string = '';
-  private categoryString: string = '';
+  public posts: Observable<Array<ForumPost>>;
+  private category: string = '';
   private d: any;
-  constructor(private router: Router, private forumService: ForumService, private dialog: MatDialog) {
+  constructor(private router: Router, 
+    private forumService: ForumService, 
+    private dialog: MatDialog, 
+    private snackbar: MatSnackBar,
+    private location: Location) {
     this.router.events.subscribe((route: RouterEvent) => {
-      try {
+      try { 
         this.currentPage = this.router.url.split("/").pop().replace(new RegExp(/-/g), ' ');
-        this.categoryString = this.router.url.split("/")[2].replace(new RegExp(/\-/g), ' ');
+        this.category = this.router.url.split("/")[2].replace(new RegExp(/\-/g), ' ');
       }
       catch(err) {
         
@@ -31,18 +40,26 @@ export class ForumTopicPageComponent implements OnInit {
   
   ngOnInit() {
     this.currentPage = this.currentPage.split(" ").map(t => this.capitalize(t)).join(" ");
-    this.categoryString = this.categoryString.split(" ").map(t => this.capitalize(t)).join(" ");
-    console.log(this.currentPage);
-    console.log(this.categoryString)
+    this.category = this.category.split(" ").map(t => this.capitalize(t)).join(" ");
     // Fetch API with current page to get all forum posts.
-    this.forumService.getForumPosts(this.categoryString, this.currentPage).subscribe((posts : Array<ForumPost>) => {
-      console.log(posts);
-    }, (err : HttpErrorResponse) => {
-      console.log(err);
-    });
+    // this.forumService.getForumPosts(this.category, this.currentPage).subscribe((posts : Array<ForumPost>) => {
+    //   this.posts = posts;
+
+    // }, (err : HttpErrorResponse) => {
+    //   console.log(err);
+    // });
+    this.posts = this.forumService.getForumPosts(this.category, this.currentPage).pipe(share());
+    console.log(this.posts)
+    // Subscribe to Events for creating posts.
     this.forumService.postDialogEvents.subscribe((event: PostEvent) => {
       console.log(event);
-      this.d.close();
+      if(event.name === 'cancelled') {
+        this.d.close();
+      }
+      else if(event.name === 'created') {
+        this.snackbar.open('Post created!', 'Dismiss', { duration: 5000 });
+        this.d.close();
+      }
     }, err => console.log(err), () => console.log("Done"))
   }
 
@@ -50,9 +67,17 @@ export class ForumTopicPageComponent implements OnInit {
     // Open Dialog with Quill Editor
     this.d = this.dialog.open(CreatePostFormComponent, {
       height: 'auto',
-      width: 'auto'
+      width: 'auto',
+      data: {
+        category: this.category,
+        topic: this.currentPage
+      }
     });
     
+  }
+
+  goBack() : void {
+    this.location.back();
   }
   private capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.substring(1);
 
