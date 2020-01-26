ff3e0d323dbbec94c70019c230db3bd41f82a20a
Anson Foong <ansfoong@gmail.com>
2020-01-25 22:56:13 -0500

    Added route for Forum Posts, updated pathMatch.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { GuestPageComponent } from './components/guest-page/guest-page.component';
 import { LogoutPageComponent } from './components/logout-page/logout-page.component';
 import { ForumTopicPageComponent } from './components/Forum/forum-topic-page/forum-topic-page.component';
+import { ForumPostPageComponent } from './components/Forum/forum-post-page/forum-post-page.component';
 
 
 const routes: Routes = [
   { path: 'guest', component: GuestPageComponent },
   { path: 'forum', pathMatch: 'full', component: ForumPageComponent },
-  { path: 'forum/:category/:topic', component: ForumTopicPageComponent },
-  { path: 'forum/:category',redirectTo: 'forum' },
+  { path: 'forum/:category/:topic', pathMatch: 'full', component: ForumTopicPageComponent },
+  { path: 'forum/:category', redirectTo: 'forum' },
   { path: 'forum/topic', redirectTo: 'forum'},
+  { path: 'forum/:category/:topic/:title/:id',  pathMatch: 'full', component: ForumPostPageComponent },
   { path: 'logout', component: LogoutPageComponent }
 ];
 
